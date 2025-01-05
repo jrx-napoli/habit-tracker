8ca38c4fd65d6ecd709c9ab20802bf5c3f82c9d1
jrx-napoli <janek.rybarczyk@gmail.com>
2025-01-05 17:29:42 +0100

    edit habit form

---
+++ habit_tracker/lib/habit_form/edit_habit_form.dart
@@ -0,0 +1,134 @@
+import 'dart:ui'; // Required for the blur effect
+import 'package:flutter/material.dart';
+
+import 'package:habit_tracker/constants/app_colors.dart';
+import 'input_field.dart';
+import 'frequency_field.dart';
+import 'target_field.dart';
+import 'notifications_field.dart';
+import 'notes_field.dart';
+import 'delete_button.dart';
+
+
+class EditHabitForm extends StatelessWidget {
+  const EditHabitForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.9,
+      maxChildSize: 0.9, // Expandable up to 90%
+      minChildSize: 0.7, // Minimum height at 70%
+      builder: (context, scrollController) {
+        return Stack(
+          children: [
+            
+            // Background blur
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+              child: Container(),
+            ),
+            
+            // Habit creation form container
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.semiTransparentBackground,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              padding: const EdgeInsets.only(left: 25, right: 25, top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  
+                  // Header with Cancel and Add buttons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(), // Close the modal
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.red, 
+                            fontSize: 16),
+                        ),
+                      ),
+                      Text(
+                        "Edit",
+                        style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w700,
+                            color: AppColors.darkGrey, 
+                            fontSize: 18),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Add habit logic here
+                          Navigator.of(context).pop(); // Close the modal
+                        },
+                        child: Text(
+                          "Done",
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w600,
+                            color: Colors.blue, 
+                            fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 10),
+
+                  // Form Fields
+                  Expanded(
+                    child: ListView(
+                      controller: scrollController, // Makes the form scrollable
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20)
+                          ),
+                          child: Column(
+                            children: [
+                              InputField(label: "Activity"),
+                              SizedBox(height: 10),
+                              InputField(label: "Category"),
+                            ],
+                          )
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20)
+                          ),
+                          child: Column(
+                            children: [
+                              FrequencyField(),
+                              SizedBox(height: 10),
+                              TargetField(),
+                              SizedBox(height: 10),
+                              NotificationsField(),
+                            ],
+                          )
+                        ),
+                        SizedBox(height: 20),
+                        NotesField(),
+                        SizedBox(height: 20),
+                        DeleteButton()
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
--- habit_tracker/lib/home_page/habit_card.dart
+++ habit_tracker/lib/home_page/habit_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../constants/app_colors.dart';
+import '../habit_form/edit_habit_form.dart';
 
 
 // Widget for Habit Cards
@@ -29,96 +30,110 @@ class HabitCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [colorA, colorB],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.dropShadowColor,
-            spreadRadius: 0,
-            blurRadius: 20,
-            offset: Offset(0, 9), // (horizontal, vertical)
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          
-          // Habit title and category
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                title,
-                style: const TextStyle(
-                  color: AppColors.darkGrey,
-                  fontSize: 16,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              HeartIcon(
-                color: colorB.withAlpha(255),
-              ),
-            ],
-          ),
-          Text(
-            category,
-            style: TextStyle(
-              color: AppColors.darkGrey.withAlpha(150),
-              fontSize: 14,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          const SizedBox(height: 16),
+    return GestureDetector(
+      onLongPress: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true, // Allows the modal to expand fully
+          backgroundColor: Colors.transparent, 
+          barrierColor: Colors.transparent,// Makes the modal background transparent
+          builder: (context) {
+            return EditHabitForm();
+          },
+        );
+      },
 
-          // Streak and Progress Bar
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  FlameIcon(),
-                  SizedBox(width: 8), // 20 pixels of space
-                  Text(
-                    streak,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w500,
-                    ),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [colorA, colorB],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.dropShadowColor,
+              spreadRadius: 0,
+              blurRadius: 20,
+              offset: Offset(0, 9), // (horizontal, vertical)
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            
+            // Habit title and category
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: AppColors.darkGrey,
+                    fontSize: 16,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w700,
                   ),
-                ],
-              ),
-              Text(
-                status,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w500,
                 ),
+                HeartIcon(
+                  color: colorB.withAlpha(255),
+                ),
+              ],
+            ),
+            Text(
+              category,
+              style: TextStyle(
+                color: AppColors.darkGrey.withAlpha(150),
+                fontSize: 14,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w500,
               ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          
-          // Progress bar
-          LinearProgressIndicator(
-            value: progress / goal,
-            backgroundColor: Colors.white38,
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-          ),
-        ],
+            ),
+            const SizedBox(height: 16),
+
+            // Streak and Progress Bar
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    FlameIcon(),
+                    SizedBox(width: 8), // 20 pixels of space
+                    Text(
+                      streak,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  status,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            
+            // Progress bar
+            LinearProgressIndicator(
+              value: progress / goal,
+              backgroundColor: Colors.white38,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
